0ec20a0fa3da25ad1840123f05c4b960edcaca20
ShahmaSilmy <fathimashahma9456@gmail.com>
2025-05-04 15:33:10 +0530

    Streamlit .py file update

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,11 +43,27 @@ def load_data():
 
 import plotly.express as px
 
+st.subheader("Total Paddy Production by Each District")
+
+#clean district name and filter out the sri lanka
+df['District'] = df['District'].str.strip().str.upper()
+filtered_df = df[df['District'] != "SRI LANKA"]
+
+all_districts = sorted(filtered_df['District'].unique())
+
+selected_destricts = st.multiselect(
+    "Select Districts to DIsplay",
+    options = all_districts,
+    default = all_districts
+)
+
+filtered_district_df = filtered_df[filtered_df['District'].isin(selected_destricts)]
+
 #Grouping by district and sum of production
-prod_by_district = df.groupby('District')['Production_MT'].sum().reset_index()
+prod_by_district = filtered_district_df.groupby('District')['Production_MT'].sum().reset_index()
 
 #sort by production for good visualisation
-prod_by_district = prod_by_district.sort_values(by="Production_MT",ascending=False)
+prod_by_district = filtered_district_df.sort_values(by="Production_MT",ascending=False)
 
 #Get the bar chart
 fig1 = px.bar(
@@ -64,11 +80,26 @@ st.plotly_chart(fig1, use_container_width=True)
 
 ##plot 2
 
+#adding a subheader
+st.subheader("National Average Paddy Yield Trend")
+
+year_range = st.slider(
+    "select year range",
+    min_value = int(df['Year'].min()),
+    max_value = int(df['Year'].max()),
+    value = (2020, 2023)
+    )
+
 # Ensure 'Yield_Average' is numeric
 avg_yield_trend = df.groupby("Year")['Yield_Average'].mean().reset_index()
 
 # Explicitly convert 'Year' to integer after grouping to remove decimal points
 avg_yield_trend['Year'] = avg_yield_trend['Year'].astype(int)
+
+avg_yield_trend = avg_yield_trend[
+    (avg_yield_trend['Year'] >= year_range[0]) &
+    (avg_yield_trend['Year'] <= year_range[1])
+]
 
 #plot using the plotly library
 fig2 = px.line(
@@ -95,13 +126,24 @@ fig2.update_layout(
 )
 st.plotly_chart(fig2, use_container_width=True)
 
-#plot 3 
-
+##plot 3 
+df['District'] = df['District'].str.strip().str.upper()
 seasonal_df = df[df["District"] != "SRI LANKA"]
+
+#The filters
+prod_year = st.selectbox("Select Year",sorted(df['Year'].astype(int).unique()))
+prod_season = st.selectbox("select Season",sorted(df['Season'].unique()))
+
+seasonal_df = df[
+    (df["District"] != "SRI LANKA") &
+    (df["Year"] == prod_year) &
+    (df["Season"] == prod_season)
+]
 
 prod_by_district = seasonal_df.groupby("District")["Production_MT"].sum().reset_index()
 
 prod_by_district = prod_by_district.sort_values(by="Production_MT",ascending=False)
+
 
 #creating the bar chart
 fig3 = px.bar(
